e7c624b709970637d71bc75c78ed500443edbcee
Kyrylo Danylov <kyrylo.danylov@yalantis.net>
2022-08-04 13:35:08 +0300

    item & feed coordinator

--- SimpleNavigation/Application/Content/Screens/Feed/FeedCoordinator.swift
+++ SimpleNavigation/Application/Content/Screens/Feed/FeedCoordinator.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-enum FeedCoordinatorNavigation {
+enum FeedCoordinatorNavigation: String {
 
     case feed
     case item
@@ -33,7 +33,36 @@ final class FeedCoordinator: Coordinator, DeeplinkHandable {
     }
 
     func handleURL(_ url: URL) -> Bool {
-        false
+        guard var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false) else { return false }
+
+        let pathComponents = urlComponents.path.components(separatedBy: "/").dropFirst() // empty
+
+        guard let currentPathComponent = pathComponents.first,
+              let coordinator = FeedCoordinatorNavigation(rawValue: currentPathComponent) else { return false }
+
+        switch coordinator {
+        case .item:
+            let queryItems = urlComponents.queryItems
+            if let itemId = queryItems?.first(where: { $0.name == "itemId" })?.value {
+                let coordinator = ItemCoordinator(itemId: itemId)
+                coordinator.onFinish = { [weak self] in
+                    self?.popFromNavigationStack()
+                }
+
+                pushToNavigationStack(.item, viewModel: coordinator)
+
+                urlComponents.path = "/".appending(pathComponents.dropFirst().joined(separator: "/"))
+
+                guard let url = urlComponents.url else { return false}
+
+                return coordinator.handleURL(url)
+            } else {
+                return false
+            }
+
+        default:
+            return false
+        }
     }
     
 }
--- SimpleNavigation/Application/Content/Screens/Item/ItemCoordinator.swift
+++ SimpleNavigation/Application/Content/Screens/Item/ItemCoordinator.swift
@@ -1,13 +1,13 @@
 import Foundation
 
-enum ItemCoordinatorNavigation {
+enum ItemCoordinatorNavigation: String {
 
     case item
     case details
 
 }
 
-final class ItemCoordinator: Coordinator {
+final class ItemCoordinator: Coordinator, DeeplinkHandable {
 
     @Published var navigationStack: [(ItemCoordinatorNavigation, Any)] = []
     
@@ -35,6 +35,26 @@ final class ItemCoordinator: Coordinator {
     private func pushItemDetails() {
         let viewModel = ItemDetailsViewModel()
         pushToNavigationStack(.details, viewModel: viewModel)
+    }
+
+    func handleURL(_ url: URL) -> Bool {
+
+        guard let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false) else { return false }
+
+        let pathComponents = urlComponents.path.components(separatedBy: "/").dropFirst() // empty
+
+        guard let currentPathComponent = pathComponents.first,
+              let coordinator = ItemCoordinatorNavigation(rawValue: currentPathComponent) else { return false }
+
+        switch coordinator {
+        case .details:
+            pushItemDetails()
+
+            return true
+
+        default:
+            return false
+        }
     }
     
 }
